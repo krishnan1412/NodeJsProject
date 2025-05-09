3cc7c8393ee25bb5e226b27cd1c4be50212b6f5a
krishnan1412 <krishnan27roman@gmail.com>
2025-05-09 10:36:26 +0530

    first update

---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const userController = require("../controllers/userController");
+
+
+router.post("/",userController.signUp);
+
+router.post("/login", userController.logIn);
+
+router.patch('/:userId', userController.updateUser);
+
+router.delete('/:userId', userController.deleteUser);
+
+router.get('/data', userController.data);
+
+module.exports = router;
+
